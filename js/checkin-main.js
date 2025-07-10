da52ca9c6508038397613a50828fe1fee90a314b
Evan Prodromou <evan@openearth.org>
2025-07-10 10:22:25 -0400

    fix: filter places with names

--- js/checkin-main.js
+++ js/checkin-main.js
@@ -45,7 +45,7 @@ class CheckinMainElement extends LitElement {
       }
 
       const collection = await res.json();
-      this._places = collection.items;
+      this._places = collection.items.filter(p => (p.name))
     });
   }
 
