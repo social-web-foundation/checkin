8b0cdc765a8eb59e4cd04ba07b81f53d4c04b39f
Evan Prodromou <evan@openearth.org>
2025-07-13 16:24:50 -0400

    fix: finally get the constructor working, sheesh

--- js/checkin-activity.js
+++ js/checkin-activity.js
@@ -62,7 +62,7 @@ export class CheckinActivityElement extends CheckinElement {
   }
 
   constructor() {
-    super.constructor()
+    super()
     this._actor = this.activity?.actor
     this._location = this.activity._location
     this._target = this.activity._target
